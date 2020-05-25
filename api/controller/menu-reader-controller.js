9e640d0f3d441964929584eb90065c6da4d7cbd3
Aneesha Chaudhury <Aneesha_Chaudhury@Dell.com>
2020-05-25 17:25:38 +0530

    ff

--- api/controller/menu-reader-controller.js
+++ api/controller/menu-reader-controller.js
@@ -23,7 +23,7 @@ exports.get_all_eatery = function(req, res) {
                 }
             })
         console.log(names);
-        res.json({fulfillmentText : response});
+        res.json({response});
     });
   };
 
